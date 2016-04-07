7ae4b06b55900decffa851e082ef83e29a849fd8
shangmingchao <smchao1993@163.com>
2016-04-08 00:29:32 +0800

    修复宽度异常Bug;修复点击屏幕边缘箭头指示器异常Bug;代码优化

--- app/src/main/java/com/frank/popuplist/PopupList.java
+++ app/src/main/java/com/frank/popuplist/PopupList.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -22,6 +23,13 @@ import java.util.List;
  * 列表项的长按弹出菜单
  */
 public class PopupList implements View.OnTouchListener {
+
+    private static final int itemViewWidth = 50;//dp
+    private static final int itemViewHeight = 40;//dp
+    private static final int dividerWidth = 1;//dp
+    private static final int arrowWidth = 15;//dp
+    private static final int arrowHeight = 7;//dp
+    private static final int windowCornersRadius = 8;//dp
 
     private volatile static PopupList instance;
 
@@ -65,8 +73,11 @@ public class PopupList implements View.OnTouchListener {
      * @param rawY      手指按下的屏幕绝对坐标Y
      */
     public void showPopupWindow(final Context context, View parent, int position, List popupList, float rawX, float rawY) {
-
         this.mContext = context;
+        //预防性Bug修复，详见http://blog.csdn.net/shangmingchao/article/details/50947418
+        if (mContext instanceof Activity && ((Activity) mContext).isFinishing()) {
+            return;
+        }
         // popupWindow要显示的内容
         ViewGroup layoutView = (ViewGroup) LayoutInflater.from(mContext).inflate(
                 R.layout.popup_list, null);
@@ -86,23 +97,31 @@ public class PopupList implements View.OnTouchListener {
         recyclerView.setAdapter(popupListAdapter);
         //设置列表分割线
         recyclerView.addItemDecoration(new DividerItemDecoration(mContext, LinearLayoutManager.HORIZONTAL));
-        //recyclerView的item view宽50dp高40dp，列表分割线宽1dp，箭头宽15dp高7dp
-        //可计算出弹出窗口的宽高
-        int PopupWindowWidth = recyclerView.getAdapter().getItemCount() * ScreenUtils.dp2px(50 + 1);
-        int PopupWindowHeight = ScreenUtils.dp2px(40 + 7);
-        //为水平列表添加箭头指示器，并计算其位置为手指按下位置
+        //计算出弹出窗口的宽高
+        int PopupWindowWidth = recyclerView.getAdapter().getItemCount() * ScreenUtils.dp2px(itemViewWidth) +
+                (recyclerView.getAdapter().getItemCount() - 1) * ScreenUtils.dp2px(dividerWidth);
+        int PopupWindowHeight = ScreenUtils.dp2px(itemViewHeight + arrowHeight);
+        //为水平列表添加指示箭头，默认在列表的左下角，根据手指按下位置绝对坐标进行位置调整
         ImageView iv = new ImageView(mContext);
         iv.setImageResource(R.drawable.popup_arrow);
         float leftEdgeOffset = rawX;
         float rightEdgeOffset = ScreenUtils.getScreenWidth(mContext) - rawX;
         if (leftEdgeOffset < PopupWindowWidth / 2) {
-            iv.setTranslationX(leftEdgeOffset - ScreenUtils.dp2px(15 / 2.0f));
+            if (leftEdgeOffset < ScreenUtils.dp2px(arrowWidth / 2.0f)) {
+                iv.setTranslationX(ScreenUtils.dp2px(windowCornersRadius));
+            } else {
+                iv.setTranslationX(leftEdgeOffset - ScreenUtils.dp2px(arrowWidth / 2.0f));
+            }
         } else if (rightEdgeOffset < PopupWindowWidth / 2) {
-            iv.setTranslationX(PopupWindowWidth - rightEdgeOffset - ScreenUtils.dp2px(15 / 2.0f));
+            if (rightEdgeOffset < ScreenUtils.dp2px(arrowWidth / 2.0f)) {
+                iv.setTranslationX(PopupWindowWidth - rightEdgeOffset - ScreenUtils.dp2px(arrowWidth / 2.0f)-ScreenUtils.dp2px(windowCornersRadius));
+            } else {
+                iv.setTranslationX(PopupWindowWidth - rightEdgeOffset - ScreenUtils.dp2px(arrowWidth / 2.0f));
+            }
         } else {
-            iv.setTranslationX(PopupWindowWidth / 2 - ScreenUtils.dp2px(15 / 2.0f));
+            iv.setTranslationX(PopupWindowWidth / 2 - ScreenUtils.dp2px(arrowWidth / 2.0f));
         }
-        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ScreenUtils.dp2px(15), ScreenUtils.dp2px(7));
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ScreenUtils.dp2px(arrowWidth), ScreenUtils.dp2px(arrowHeight));
         layoutView.addView(iv, layoutParams);
         //实例化弹出窗口并显示
         popupListWindow = new PopupWindow(layoutView, PopupWindowWidth,
@@ -110,10 +129,6 @@ public class PopupList implements View.OnTouchListener {
         popupListWindow.setTouchable(true);
         //设置背景以便在外面包裹一层可监听触屏等事件的容器
         popupListWindow.setBackgroundDrawable(new BitmapDrawable());
-        //预防性Bug修复，详见http://blog.csdn.net/shangmingchao/article/details/50947418
-        if (mContext instanceof Activity && ((Activity) mContext).isFinishing()) {
-            return;
-        }
         //确保可以显示后，进行最终的显示
         popupListWindow.showAtLocation(parent, Gravity.CENTER,
                 (int) rawX - ScreenUtils.getScreenWidth(mContext) / 2,
@@ -133,17 +148,24 @@ public class PopupList implements View.OnTouchListener {
         }
     }
 
+    public PopupWindow getPopupListWindow() {
+        return popupListWindow;
+    }
+
     /**
      * 给ListView绑定长按弹出(在手指按下的位置)横向气泡式菜单
      *
      * @param context                  上下文，一般为Activity
-     * @param listView                 要弹出菜单的ListView
+     * @param absListView                 要弹出菜单的ListView或GridView等
      * @param popupMenuItemList        要弹出的菜单项列表
      * @param popupListOnClickListener 弹出菜单的点击监听接口
      */
-    public void initPopupList(final Context context, final ListView listView, final List<String> popupMenuItemList, PopupListAdapter.OnPopupListClickListener popupListOnClickListener) {
-        listView.setOnTouchListener(this);
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+    public void initPopupList(final Context context, final AbsListView absListView, final List<String> popupMenuItemList, PopupListAdapter.OnPopupListClickListener popupListOnClickListener) {
+        if (absListView == null || popupMenuItemList == null || popupMenuItemList.isEmpty()) {
+            return;
+        }
+        absListView.setOnTouchListener(this);
+        absListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
                 showPopupWindow(context, view, position, popupMenuItemList, rawX, rawY);
--- app/src/main/java/com/frank/sample/MainActivity.java
+++ app/src/main/java/com/frank/sample/MainActivity.java
@@ -2,6 +2,7 @@ package com.frank.sample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
